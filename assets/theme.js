f91144f0f7e9c5bdc03dfd8231dba5ac1c9214bf
Max Schmeling <max@clipisode.com>
2021-08-27 15:37:20 -0500

    Add value updating for progress circle

--- assets/theme.js
+++ assets/theme.js
@@ -6,10 +6,30 @@ setInterval(() => {
     const uploadingText = heading.innerHTML;
 
     if (uploadingText.startsWith("Uploading")) {
-      const pct = parseInt(uploadingText.split(" ")[1].split("%")[0])
+      const val = parseInt(uploadingText.split(" ")[1].split("%")[0])
       // const pct = cont.dataset.pct;
 
-      console.log({ pct })
+      // console.log({ pct })
+
+      // var val = parseInt($(this).val());
+      var $circle = document.querySelector('#svg #bar');
+      
+      if (isNaN(val)) {
+      val = 100; 
+      }
+      else{
+        var r = $circle.attr('r');
+        var c = Math.PI*(r*2);
+      
+        if (val < 0) { val = 0;}
+        if (val > 100) { val = 100;}
+        
+        var pct = ((100-val)/100)*c;
+       
+        $circle.style.strokeDashoffset = pct;
+        // $circle.css({ strokeDashoffset: pct});
+        
+        document.querySelector("#cont").setAttribute("data-pct", val);// $('#cont').attr('data-pct',val);
     }
   }
 }, 1000)

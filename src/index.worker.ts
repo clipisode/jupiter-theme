d7cb434821bfc9387d0a1b30a7cae770c4879ac0
Max Schmeling <max@clipisode.com>
2021-08-27 16:05:39 -0500

    Removes uploading text

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -45,7 +45,6 @@ export const siteData: any = {
     nameScreenHeader: `
       <div class="my-2 flex flex-col items-center">
         <div id="cont" data-pct="0" style="display:none">
-        <h1 class="text-center font-heading font-extrabold p-0 text-2xl">Uploading...</h1>
           <svg id="svg" width="200" height="200" viewPort="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <circle r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
             <circle id="bar" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>

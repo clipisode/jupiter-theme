05bdf35a1943c4bb604c39b99cb3f82176e3d3ea
Max Schmeling <max@clipisode.com>
2021-08-27 15:28:34 -0500

    Uses UPLOAD_PERCENTAGE in #cont

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -45,7 +45,7 @@ export const siteData: any = {
     nameScreenHeader: `
       <div class="my-2 flex flex-col items-center">
         <img src="THEME_FILE_ROOT/icon.png" class="h-24" />
-        <div id="cont" data-pct="100">
+        <div id="cont" data-pct="UPLOAD_PERCENTAGE">
           <svg id="svg" width="200" height="200" viewPort="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <circle r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
             <circle id="bar" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>

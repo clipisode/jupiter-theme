20c2455cd40a2ee32ffa77e4232267464bb7f204
Brian Alvey <github@subs.frankensite.com>
2021-08-30 11:39:45 -0500

    circle design

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -45,9 +45,12 @@ export const siteData: any = {
     nameScreenHeader: `
       <div class="my-2 flex flex-col items-center">
         <div id="cont" data-pct="0" style="display:none">
-          <svg id="svg" width="200" height="200" viewPort="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <circle r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
-            <circle id="bar" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
+          <h1 id="nameScreenHeading" className="text-center font-extrabold p-0 text-2xl">
+            Uploading…
+          </h1>
+          <svg id="svg" width="130" height="130" viewPort="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <circle r="54" cx="65" cy="65" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
+            <circle id="bar" r="54" cx="65" cy="65" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
           </svg>
         </div>
       </div>
